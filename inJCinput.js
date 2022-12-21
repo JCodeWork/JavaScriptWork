d6d22e8324d430011017056ee5868c77004d704b
JCodeWork <121192973+JCodeWork@users.noreply.github.com>
2022-12-21 21:47:17 +0300

    Create inJCinput.js

---
+++ inJCinput.js
@@ -0,0 +1,4 @@
+const workJCode = 'Installing JCode...'
+const workJCode2 = 'Upgrading JCode to the lastest version...'
+const workJCode3 = 'Install unknown codes from JCode...'
+setTimeout(() => {  console.clear() }, 5000);
